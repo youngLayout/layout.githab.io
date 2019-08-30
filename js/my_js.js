691576702a8328cc69ffb71ff6a9b465438e31bc
youngLayout <53971989+youngLayout@users.noreply.github.com>
2019-08-30 09:35:41 +0300

    Add files via upload

---
+++ js/my_js.js
@@ -0,0 +1,11 @@
+$(function(){
+  'use strict';
+
+  $('.slick-slider').slick({
+    slidesToShow: 1,   /*Показывает по 3 картинки*/
+    slidesToScroll: 1,   //прокручивает по 1 картинке
+    // autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+});
